4d0bfba6456adb22471ba155dc43b2019b94aa38
Pragnesh Suthar <85042722+pragnesh-aus@users.noreply.github.com>
2021-11-22 22:18:43 +1100

    Added Nav Bar in app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,15 +12,29 @@
 <nav class="p-6 bg-white flex justify-between">
 <ul class="flex items-center">
     <li>
-        <a href="#">Home</a>
+        <a href="#" class="p-3">Home</a>
     </li>
     <li>
-        <a href="#">Dashboard</a>
+        <a href="#" class="p-3">Dashboard</a>
     </li>
     <li>
-        <a href="#">Post</a>
+        <a href="#" class="p-3">Post</a>
     </li>
 </ul>
+    <ul class="flex items-center">
+        <li>
+            <a href="#" class="p-3">User Name</a>
+        </li>
+        <li>
+            <a href="#" class="p-3">Login</a>
+        </li>
+        <li>
+            <a href="#" class="p-3">Register</a>
+        </li>
+        <li>
+            <a href="#" class="p-3">Logout</a>
+        </li>
+    </ul>
 </nav>
 
 @yield('content')
