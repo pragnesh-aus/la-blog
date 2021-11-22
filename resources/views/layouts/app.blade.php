63f3efa3727f9ca5b49b4ea86e98cc138a34febb
Pragnesh Suthar <85042722+pragnesh-aus@users.noreply.github.com>
2021-11-22 21:52:38 +1100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,17 @@
 </head>
 <body class="bg-gray-100">
 <nav class="p-6 bg-white flex justify-between">
-
+<ul class="flex items-center">
+    <li>
+        <a href="#">Home</a>
+    </li>
+    <li>
+        <a href="#">Dashboard</a>
+    </li>
+    <li>
+        <a href="#">Post</a>
+    </li>
+</ul>
 </nav>
 
 @yield('content')
