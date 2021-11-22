9d99aac080f47b75f9824ac57f11536d9de59cd0
Pragnesh Suthar <85042722+pragnesh-aus@users.noreply.github.com>
2021-11-22 22:23:17 +1100

    update design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,19 +8,19 @@
     <title>LA-Blog</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
-<body class="bg-gray-100">
-<nav class="p-6 bg-white flex justify-between">
-<ul class="flex items-center">
-    <li>
-        <a href="#" class="p-3">Home</a>
-    </li>
-    <li>
-        <a href="#" class="p-3">Dashboard</a>
-    </li>
-    <li>
-        <a href="#" class="p-3">Post</a>
-    </li>
-</ul>
+<body class="bg-gray-200 ">
+<nav class="p-6 bg-white flex justify-between mb-6">
+    <ul class="flex items-center">
+        <li>
+            <a href="#" class="p-3">Home</a>
+        </li>
+        <li>
+            <a href="#" class="p-3">Dashboard</a>
+        </li>
+        <li>
+            <a href="#" class="p-3">Post</a>
+        </li>
+    </ul>
     <ul class="flex items-center">
         <li>
             <a href="#" class="p-3">User Name</a>
