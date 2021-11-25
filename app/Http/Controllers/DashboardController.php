9e8426894b798d575cc5744d42bc2e6d02625bcd
Pragnesh Suthar <85042722+pragnesh-aus@users.noreply.github.com>
2021-11-25 18:55:49 +1100

    added email functionality based on like by user

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\PostLiked;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class DashboardController extends Controller
 {
@@ -15,6 +17,7 @@ class DashboardController extends Controller
 
     public function index(){
        // dd(auth()->user());
+
         return view('dashboard');
     }
 }
---
+++ app/Mail/PostLiked.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PostLiked extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $liker;
+    public $post;
+
+    public function __construct(User $liker, Post $post)
+    {
+        //
+        $this->liker=$liker;
+        $this->post=$post;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.posts.post_liked')->subject('someone liked your post, please have look it.');
+    }
+}
